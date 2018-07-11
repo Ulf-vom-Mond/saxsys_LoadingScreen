8514167c68a63bd35c9ee2bd14e9ea4341495bd1
Rullmann <noah.rullmann@saxsys.de>
2018-07-11 10:07:11 +0200

    some beautiful style changes

--- randomInstaImage.js
+++ randomInstaImage.js
@@ -1,8 +1,11 @@
+var token = "1384889485.1677ed0.8b6beaad74544bc799e81a67d5fc1946";
+var user_id = "1384889485";
 var IDs = [];
 var randomImageID = 0;
+var imageSize = '1000';
 var testFeed = new Instafeed({
 	get: 'user',
-	userId: '1384889485',
+	userId: user_id,
 	filter: function(image) {
 		IDs.push(image.id);
 		return false;
@@ -11,19 +14,23 @@ var testFeed = new Instafeed({
 		randomImageID = IDs[Math.floor(Math.random() * (IDs.length))];
 		userFeed.run();
 	},
-	accessToken: '1384889485.1677ed0.8b6beaad74544bc799e81a67d5fc1946'
+	accessToken: token
 });
 var userFeed = new Instafeed({
 	get: 'user',
-	userId: '1384889485',
+	userId: user_id,
 	resolution: 'standard_resolution',
+	template: '<img id="picture" src="{{image}}" filter: blur(5px)></img>',
 	filter: function(image) {
 		if(image.id == randomImageID) {
-			return true;
+			return true;			
 		}else{
 			return false;
 		}
 	},
-	accessToken: '1384889485.1677ed0.8b6beaad74544bc799e81a67d5fc1946'
+	after: function () {		
+		document.getElementById("picture").height = window.innerHeight * 0.96;
+	},
+	accessToken: token
 });
 testFeed.run();
